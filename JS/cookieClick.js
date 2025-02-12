749d8219ec380430545b7a3843b3820ea8ea8b11
Vincent <buntvincent@gmail.com>
2025-02-12 11:24:53 +0100

    CSS animatie verbeterd
    Animatie die snapte is nu gefixt.
    Cookie.kra verwijderd
    Wat onnodige CSS weggehaalt

--- JS/cookieClick.js
+++ JS/cookieClick.js
@@ -1,7 +1,11 @@
+<<<<<<< Updated upstream
 // Global variables
 let cookieCount = 0;
 let autoClickers = 0;
 let cookieMultiplier = 1;
+=======
+let cookieCount = 0;
+>>>>>>> Stashed changes
 
 const cookieCountDisplay = document.getElementById("cookieCount");
 
@@ -9,4 +13,8 @@ const cookieCountDisplay = document.getElementById("cookieCount");
 function addACookie() {
     cookieCount += cookieMultiplier;  // Add cookies based on multiplier
     cookieCountDisplay.textContent = "Cookies: " + cookieCount;
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes

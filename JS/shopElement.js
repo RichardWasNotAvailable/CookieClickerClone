6e5611c4beb223f76d59cc2774ad0294a92e56b5
Vincent <buntvincent@gmail.com>
2025-02-14 13:20:26 +0100

    twee JS bestanden samengevoegd
    Oop zou hierdoor nu goed moeten werken

--- JS/shopElement.js
+++ JS/shopElement.js
@@ -1,87 +1,78 @@
-// Initial prices and values
-let autoClickerPrice = 10;
+// Initial prices
+var autoClickerPrice = 10;
 let cookieMultiplierPrice = 50;
 let cookieBakerPrice = 100;
-let cookieFactoryPrice = 10000; 
-let cookieMultiplier = 1;
-let cookieBakers = 1;
-let cookieFactory = 0;
-let autoClickers = 0;
-let cookieCount = 0;  // Starting cookie count
-let cookieCountDisplay = document.getElementById("cookieCountDisplay"); // Assuming this is the element displaying cookies
+var cookieCount = 0;
+var autoClickers = 0;
+var cookieMultiplier = 1;
+var cookieBakers = 0;
 
-// Add a cookie with each click
-function addACookie() {
-    cookieCount += cookieMultiplier;  // Add cookies based on multiplier
-    cookieCountDisplay.textContent = "Cookies: " + cookieCount;
-}
+// Reference to the cookie counter display
+let cookieCountDisplay = document.getElementById("cookieCount");
 
-// Buy an item from the shop
-function buyItem(item) {
-    if (item === 'autoClicker') {
-        if (cookieCount >= autoClickerPrice) {
-            cookieCount -= autoClickerPrice;
-            autoClickers += 1;
+class Upgrade {
+    constructor(itemRef, priceRef, priceFactor, increaseAmount, priceVarName) {
+        this.itemRef = itemRef; // Reference to the global variable (autoClickers, cookieMultiplier, etc.)
+        this.priceRef = priceRef; // Reference to the global price variable
+        this.priceFactor = priceFactor;
+        this.increaseAmount = increaseAmount;
+        this.priceVarName = priceVarName; // Name of the price variable to update globally
+    }
+
+    buyUpgrade() {
+        if (cookieCount >= this.priceRef) {
+            cookieCount -= this.priceRef;
+            this.itemRef += this.increaseAmount; // Increase the upgrade amount
+            this.priceRef = Math.floor(this.priceRef * this.priceFactor); // Update price
+
             cookieCountDisplay.textContent = "Cookies: " + cookieCount;
 
-            // Increase the price of the autoClicker for the next purchase
-            autoClickerPrice = Math.floor(autoClickerPrice * 2); 
-            updateShopPrices();  // Update the price display in the shop
+            // Update the global price variable dynamically
+            if (this.priceVarName === "autoClickerPrice") {
+                autoClickerPrice = this.priceRef;
+            } else if (this.priceVarName === "cookieMultiplierPrice") {
+                cookieMultiplierPrice = this.priceRef;
+            } else if (this.priceVarName === "cookieBakerPrice") {
+                cookieBakerPrice = this.priceRef;
+            }
 
-            startAutoClicker();
-        } else {
-            alert('Not enough cookies!');
-        }
-    } else if (item === 'cookieMultiplier') {
-        if (cookieCount >= cookieMultiplierPrice) {
-            cookieCount -= cookieMultiplierPrice;
-            cookieMultiplier *= 2;  // Doubles the cookie multiplier
-            cookieCountDisplay.textContent = "Cookies: " + cookieCount;
-
-            // Increase the price of the cookieMultiplier for the next purchase
-            cookieMultiplierPrice = Math.floor(cookieMultiplierPrice * 152);
-            updateShopPrices();  // Update the price display in the shop
-        } else {
-            alert('Not enough cookies!');
-        }
-    } else if (item === 'cookieBaker') {
-        if (cookieCount >= cookieBakerPrice) {
-            cookieCount -= cookieBakerPrice;
-            cookieBakers *= 2;  // Double the number of bakers
-            cookieCountDisplay.textContent = "Cookies: " + cookieCount;
-
-            cookieBakerPrice = Math.floor(cookieBakerPrice * 152);
-            updateShopPrices();  // Update the price display in the shop
-        } else {
-            alert('Not enough cookies!');
-        }
-    } else if (item === 'cookieFactory') {
-        if (cookieCount >= cookieFactoryPrice) {
-            cookieCount -= cookieFactoryPrice;
-            cookieFactory *= 2;  // Double the cookie factory
-            cookieCountDisplay.textContent = "Cookies: " + cookieCount;
-
-            cookieFactoryPrice = Math.floor(cookieFactoryPrice * 152);
-            updateShopPrices();  // Update the price display in the shop
+            updateShopPrices();
         } else {
             alert('Not enough cookies!');
         }
     }
 }
 
-// Start the auto-clicker
-function startAutoClicker() {
-    setInterval(() => {
-        cookieCount += autoClickers;
-        cookieCountDisplay.textContent = "Cookies: " + cookieCount;
-    }, 2000);  // Adds a cookie every 2 seconds
+function addACookie(){
+    cookieCount += cookieMultiplier;
+    cookieCountDisplay.textContent = "Cookies: " + cookieCount;
 }
 
-// Update the prices displayed in the shop
 function updateShopPrices() {
-    // Update the displayed price in the shop for each item
     document.getElementById("autoClickerPrice").textContent = "Price: " + autoClickerPrice + " Cookies";
     document.getElementById("cookieMultiplierPrice").textContent = "Price: " + cookieMultiplierPrice + " Cookies";
     document.getElementById("cookieBakerPrice").textContent = "Price: " + cookieBakerPrice + " Cookies";
-    document.getElementById("cookieFactoryPrice").textContent = "Price: " + cookieFactoryPrice + " Cookies";
 }
+
+function buyItem(item) {
+    let newUpgrade;
+
+    if (item === 'autoClicker') {
+        newUpgrade = new Upgrade(autoClickers, autoClickerPrice, 3, 1, "autoClickerPrice");
+        newUpgrade.buyUpgrade();
+        autoClickers++; // Make sure the global value increases
+    } else if (item === 'cookieMultiplier') {
+        newUpgrade = new Upgrade(cookieMultiplier, cookieMultiplierPrice, 2.5, 2, "cookieMultiplierPrice");
+        newUpgrade.buyUpgrade();
+        cookieMultiplier++; 
+    } else if (item === 'cookieBaker') {
+        newUpgrade = new Upgrade(cookieBakers, cookieBakerPrice, 152, 2, "cookieBakerPrice");
+        newUpgrade.buyUpgrade();
+        cookieBakers++;
+    }
+}
+
+setInterval(() => {
+    cookieCount += autoClickers;
+    cookieCountDisplay.textContent = "Cookies: " + cookieCount;
+}, 2000);
